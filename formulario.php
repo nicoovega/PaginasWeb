8e620059b1401ea588f318c0390c9c1aa9818ceb
nicoovega <91068537+nicoovega@users.noreply.github.com>
2021-10-18 11:10:47 -0300

    Add files via upload

--- formulario.php
+++ formulario.php
@@ -1,15 +1,15 @@
 <?php
-    $Nombre= $_REQUEST ['Nombre']
-    $contraseña= $_REQUEST ['contraseña']
-    $opciones = $_REQUEST ['Opciones']
+    $Nombre= $_REQUEST ['Nombre'];
+    $contraseña= $_REQUEST ['contraseña'];
+    $opciones = $_REQUEST ['Opciones'];
     if ($opciones==0)
-    echo "Tu personaje es Homero"
+    echo "Tu personaje es Homero";
     elseif ($opciones==1)
-    echo "Tu personaje es Bart"
+    echo "Tu personaje es Bart";
     elseif ($opciones==2)
-    echo "Tu personaje es Lisa"
+    echo "Tu personaje es Lisa";
     elseif ($opciones==3)
-    echo "Tu personaje es Marge"
+    echo "Tu personaje es Marge";
     elseif ($opciones==4)
-    echo "Tu personaje favorito es otro"
+    echo "Tu personaje favorito es otro";
     ?>
